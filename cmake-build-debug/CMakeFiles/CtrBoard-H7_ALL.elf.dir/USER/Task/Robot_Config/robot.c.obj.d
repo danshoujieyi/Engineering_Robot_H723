fef8f0eaed28fb217331a72bbe226c96f093b7a5
danshoujieyi <121175903+danshoujieyi@users.noreply.github.com>
2025-06-05 02:21:24 +0800

    第二十五次提交，Add .gitignore to ignore build files

--- cmake-build-debug/CMakeFiles/CtrBoard-H7_ALL.elf.dir/USER/Task/Robot_Config/robot.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/CtrBoard-H7_ALL.elf.dir/USER/Task/Robot_Config/robot.c.obj: \
- D:\GitHub_Project\Engineering_Robot_H723\USER\Task\Robot_Config\robot.c \
- D:\GitHub_Project\Engineering_Robot_H723\USER\Task\Robot_Config\robot.h \
- D:\GitHub_Project\Engineering_Robot_H723\USER\Task\USART/usart_task.h
--- cmake-build-debug/CMakeFiles/CtrBoard-H7_ALL.elf.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for CtrBoard-H7_ALL.elf.
